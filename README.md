41d4cefc9864d3539f38a8a42a5f0b7c34f42ac5
Mike Davis <mike@michaeljacobdavis.com>
2015-08-19 20:19:27 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### Sweet Sweet Proxy and Echo Server
+
+#### Run
+```
+npm install
+bodemon index.js  [options]
+```
+
+#### Options
+Option | Default
+--- | ---
+`--port` | 80 (8080 if localhost)
+`--host` | 127.0.0.1
+`--url` |
+`--log` | Log to `process.stdout`
+
+
+#### Demo
+![](walkthrough.gif)
--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-let http = require('http');
-let request = require('request');
+let http = require('http')
+let request = require('request')
+let path = require('path')
+let fs = require('fs')
+let argv = require('yargs')
+  .default('host', '127.0.0.1')
+  .argv
+
+let scheme = 'http://'
+let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let logPath = argv.log && path.join(__dirname, argv.log)
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
 
 // Echo Server
 http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`);
-    req.pipe(res);
-    for (let header in req.headers) {
-      res.setHeader(header, req.headers[header]);
-    }
+  console.log(`Request received at: ${req.url}`)
+  req.pipe(res)
+
+  for (let header in req.headers) {
+    res.setHeader(header, req.headers[header])
+  }
 }).listen(8000)
 
-let destinationUrl = '127.0.0.1:8000';
-
 http.createServer((req, res) => {
+
+  if (req.headers['x-destination-url']) {
+    destinationUrl = req.headers['x-destination-url']
+  }
+
+  // Logging
+  logStream.write('Request headers: ' + JSON.stringify(req.headers))
   console.log(`Proxying request to: ${destinationUrl + req.url}`)
+
   // Proxy code
-    let options = {
-        headers: req.headers,
-        url: `http://${destinationUrl}${req.url}`
-    };
-    options.method = req.method;
-    req.pipe(request(options)).pipe(res);
+  let options = {
+    headers: req.headers,
+    url: `${destinationUrl}${req.url}`
+  }
+  options.method = req.method
+
+  // Log response
+  let downstreamResponse = req.pipe(request(options))
+  process.stdout.write(JSON.stringify(downstreamResponse.headers))
+  downstreamResponse.pipe(logStream, {end: false})
+  downstreamResponse.pipe(res)
 }).listen(8001)
